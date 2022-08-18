833f742e7e925398b996c74e9121eb02915bc3fb
izdanii <faizsukmadanii@gmail.com>
2022-08-18 22:38:39 +0700

    register-Firebase

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -1,34 +1,94 @@
-import "./register.css"
+import "./register.css";
 import { NavLink } from "react-router-dom";
 import Header from "components/header/header";
+import { auth, registerWithEmailAndPassword } from "config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Register = () => {
-    return(
-      <>
+  const [user, loading, error] = useAuthState(auth);
+  const navigate = useNavigate();
+  const [signUp, setSignUp] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+
+  const handleSubmit = async (e, params) => {
+    e.preventDefault();
+    if (params === "register") {
+      console.log(signUp.email, signUp.password, signUp.name);
+      await registerWithEmailAndPassword(signUp.name, signUp.email, signUp.password);
+    }
+  };
+  useEffect(() => {
+    if (loading) {
+      return;
+    }
+    if (user) {
+      navigate("/");
+    }
+    if (error) {
+      alert(error);
+    }
+  }, [loading, user, error, navigate]);
+
+  return (
+    <>
       <Header />
-        <div className="account-page">
+      <div className="account-page">
         <div className="container">
           <div className="row">
             <div className="col-2">
               <div className="form-container">
                 <div className="form-btn">
-                  <span onclick="login()">Register</span>
+                  <span>Register</span>
                 </div>
                 <form action="" id="RegForm">
-                    <input type="text" placeholder="Username" />
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
-    
-                    <button type="submit" className="btn">Register</button>
-                    <NavLink to = "/login">Switch To Login</NavLink>
-                  </form>
+                  <input
+                    type="text"
+                    onChange={(e) =>
+                      setSignUp({ ...signUp, name: e.target.value })
+                    }
+                    placeholder="Username"
+                    value={signUp.name}
+                  />
+
+                  <input
+                    type="email"
+                    onChange={(e) =>
+                      setSignUp({ ...signUp, email: e.target.value })
+                    }
+                    placeholder="Email"
+                    value={signUp.email}
+                  />
+
+                  <input
+                    type="password"
+                    onChange={(e) =>
+                      setSignUp({ ...signUp, password: e.target.value })
+                    }
+                    placeholder="Password"
+                    value={signUp.password}
+                  />
+
+                  <button
+                    type="submit"
+                    onClick={(e) => handleSubmit(e, "register")}
+                    className="btn"
+                  >
+                    Register
+                  </button>
+                  <NavLink to="/login">Switch To Login</NavLink>
+                </form>
               </div>
             </div>
           </div>
         </div>
       </div>
-      </>
-    )
-}
+    </>
+  );
+};
 
 export default Register;
--- src/config/firebase/index.js
+++ src/config/firebase/index.js
@@ -1,32 +1,46 @@
 import { initializeApp } from "firebase/app";
 import {
-    getAuth,
-    signInWithEmailAndPassword,
-    createUserWithEmailAndPassword,
-    signOut
-  } from 'firebase/auth';
-import {getFirestore} from "firebase/firestore";
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { getFirestore, addDoc, collection } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDA8LoHNeUIIOZDqKrQOgJPajp32xGsKCg",
-    authDomain: "frontend-ecommerce-f6569.firebaseapp.com",
-    projectId: "frontend-ecommerce-f6569",
-    storageBucket: "frontend-ecommerce-f6569.appspot.com",
-    messagingSenderId: "430329970428",
-    appId: "1:430329970428:web:f208b34f4bedacc9be6179"
-  };
+  apiKey: "AIzaSyDA8LoHNeUIIOZDqKrQOgJPajp32xGsKCg",
+  authDomain: "frontend-ecommerce-f6569.firebaseapp.com",
+  projectId: "frontend-ecommerce-f6569",
+  storageBucket: "frontend-ecommerce-f6569.appspot.com",
+  messagingSenderId: "430329970428",
+  appId: "1:430329970428:web:f208b34f4bedacc9be6179",
+};
 
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-  const db = getFirestore(app);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-  const logInWithEmailAndPassword = async (email, password) => {
-    try {
-      await signInWithEmailAndPassword(auth, email, password);
-    } catch (err) {
-      console.error(err);
-      alert(err.message);
-    }
-  };
+const logInWithEmailAndPassword = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
 
-  export {auth,db, logInWithEmailAndPassword }
+const registerWithEmailAndPassword = async (name, email, password) => {
+  console.log("register");
+  const response = await createUserWithEmailAndPassword(auth, email, password);
+  console.log("response", response);
+  const user = response.user;
+  await addDoc(collection(db, "users"), {
+    uid: user.uid,
+    name,
+    authProvider: "local",
+    email,
+    password,
+  });
+};
+
+export { auth, db, registerWithEmailAndPassword, logInWithEmailAndPassword };
